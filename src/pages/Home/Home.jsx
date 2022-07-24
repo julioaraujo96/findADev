fd4bbe2961d51926b7ed64b9deab705516f1c363
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2022-07-24 23:24:09 +0100

    initial styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,26 @@ import React, { useState } from 'react'
 import './styles.scss'
 
 const Home = () => {
-    return (
-        <div className='home-page'>
-            
-        </div>
-    );
+  return (
+    <div className="home-page">
+      <div className="main-container">
+        <header>
+          <h1>findADev.</h1>
+          <span>Light</span>
+        </header>
+        <form className="inputContainer">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search Github Username..."
+          />
+          <button type="submit" className="searchButton">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
