62a8036f82951b6874e0d9d603c7120111013529
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2022-07-28 00:14:55 +0100

    install react router + handling submit logic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
-import Card from "../../Components/Card/Card";
 import Header from "../../Components/Header/Header";
 import "./styles.scss";
 
 const Home = () => {
+  //state
+  const [searchText, setSearchText] = useState('');
+
+  const handleSubmit = () => {
+    e.preventDefault()
+    if(searchText && searchText.trim()){
+      //todo: add routing and push search to /search/:user
+    }
+  };
+
   return (
     <div className="home-page">
       <div className="main-container">
         <Header />
-        <form className="inputContainer">
+        <form className="input-container" onSubmit={() => handleSubmit()}>
           <input
             type="text"
             className="search-input"
             placeholder="Search Github Username..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
           />
-          <button type="submit" className="searchButton">
+          <button type="submit" className="search-button">
             Search
           </button>
         </form>
-        <Card>
-            <h1>hello</h1>
-        </Card>
       </div>
     </div>
   );
