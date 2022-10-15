a6e5619c1ae0ab917414b0cea3ea127d4aa8cb78
julioaraujo96 <julioaraujo.bs@gmail.com>
2022-10-15 21:00:36 +0100

    coloring update

--- src/Components/UI/Button/Button.jsx
+++ src/Components/UI/Button/Button.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import {useContext} from 'react';
 import "./styles.scss";
 
+import { ThemeContext } from "../../../pages/App/App";
+
 function Button({type,modifier,children}) {
+const { theme, setTheme } = useContext(ThemeContext);
+  console.log(theme)
     return (
-        <button type={type} className={`${modifier}-button`}>
+        <button type={type} className={`${modifier}-button btn-${theme}`}>
             {children}
         </button>
     );
